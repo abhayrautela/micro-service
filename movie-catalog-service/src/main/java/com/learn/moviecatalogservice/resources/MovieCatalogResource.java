9225de38129265ca395b13458070716f1bc41281
abhayrautela <abhayrautela@gmail.com>
2019-04-28 16:46:27 +0530

    Added business logic to the microservices

---
+++ movie-catalog-service/src/main/java/com/learn/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,36 @@
+package com.learn.moviecatalogservice.resources;
+
+import com.learn.moviecatalogservice.models.CatalogItem;
+import com.learn.moviecatalogservice.models.Movie;
+import com.learn.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        return userRating.getUserRating().stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
