5f7115bc6fdad44b8e3a28f9c2b321ea023ea0b0
abhayrautela <abhayrautela@gmail.com>
2019-04-29 01:22:31 +0530

    Added service discovery using spring cloud

--- movie-catalog-service/src/main/java/com/learn/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/learn/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,12 +2,14 @@ package com.learn.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+    }
 
 }
--- ratings-data-service/src/main/java/com/learn/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/learn/ratingsdataservice/RatingsDataServiceApplication.java
@@ -2,12 +2,14 @@ package com.learn.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingsDataServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingsDataServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsDataServiceApplication.class, args);
+    }
 
 }
